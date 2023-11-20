3f75863fa225241c856b742e0faa227dd5571b06
nstsmalinovskaa <nstsmalinovskaa@gmail.com>
2023-11-20 09:24:49 +0300

    init

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+import './bootstrap';
+import { createApp } from 'vue';
+import App from "./components/App.vue";
+
+// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+// });
+
+createApp(App).mount('#app');
---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ERP</title>
+
+        <link href="{{asset('css/app.css')}}" ref="stylesheet">
+        <link href="{{asset('css/reset.css')}}" ref="stylesheet">
+
+        <script type="module" src="{{asset('js/app.js')}}" defer></script>
+        @vite('resources/css/app.css')
+        @vite('resources/css/reset.css')
+        @vite('resources/img/logo.css')
+    </head>
+    <body>
+        <div id="app" style="width: 100%"></div>
+        @vite('resources/js/app.js')
+    </body>
+</html>
